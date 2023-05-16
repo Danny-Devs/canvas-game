edf91d6508f61e3fc692059b8121c53079ae92f8
Daniel Ahn <et3rnal.d@gmail.com>
2023-05-15 20:28:40 -0700

    30:19 in

--- index.js
+++ index.js
@@ -1,5 +1,69 @@
-const canvas = document.getElementById('canvas');
+const canvas = document.getElementById('canvas')
 
-canvas.width = innerWidth;
-canvas.height = innerHeight;
+canvas.width = innerWidth
+canvas.height = innerHeight
 
+const c = canvas.getContext('2d')
+
+class Player {
+  constructor(x, y, radius, color) {
+    this.x = x
+    this.y = y
+    this.radius = radius
+    this.color = color
+  }
+
+  draw() {
+    c.beginPath()
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+    c.fillStyle = this.color
+    c.fill()
+  }
+}
+
+class Projectile {
+  constructor(x, y, radius, color, velocity) {
+    this.x = x
+    this.y = y
+    this.radius = radius
+    this.color = color
+    this.velocity = velocity
+  }
+  draw() {
+    c.beginPath()
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+    c.fillStyle = this.color
+    c.fill()
+  }
+  update() {
+    this.draw()
+    this.x = this.x + this.velocity.x
+    this.y = this.y + this.velocity.y
+  }
+}
+
+const x = canvas.width / 2
+const y = canvas.height / 2
+const player = new Player(x, y, 30, 'blue')
+player.draw()
+
+const projectile = new Projectile(canvas.width / 2, canvas.height / 2, 5, 'red', { x: 1, y: 1 })
+
+function animate() {
+  requestAnimationFrame(animate)
+  c.clearRect(0, 0, canvas.width, canvas.height)
+  player.draw()
+  projectile.draw()
+  projectile.update()
+}
+
+addEventListener('click', (e) => {
+  const angle = Math.atan2(e.clientY - canvas.height / 2, e.clientX - canvas.width / 2)
+  const velocity = {
+    x: Math.cos(angle),
+    y: Math.sin(angle),
+  }
+  projectile.velocity = velocity
+})
+
+animate()
